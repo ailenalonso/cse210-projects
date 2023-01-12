92d79c50f8632ab46d8252647b2e9bbc77c865ea
ailenalonso <ailu.alonso@gmail.com>
2023-01-12 18:02:38 -0300

    Prep 2 changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse (gradePercentage);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Yor grade is: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine ("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine ("I'm sorry you did not pass the coure, you will do it great next time");
+        }
+
+
+
     }
 }
