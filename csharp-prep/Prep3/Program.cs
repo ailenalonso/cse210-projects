5a4226692a3585be896b95c2b8ead3dbf431a10c
ailenalonso <ailu.alonso@gmail.com>
2023-01-12 23:12:35 -0300

    Prep 3 changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+        int userNumber = 0;
+
+        while (userNumber != number)
+        {
+            Console.Write("What is your guess? ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (number > userNumber)
+            {
+                Console.WriteLine ("Higher");
+            }
+            else if (number < userNumber)
+            {
+                Console.WriteLine ("Lower");
+            }
+            else
+            {
+                Console.WriteLine ("You guessed it!");
+            }
+
+        }
+
     }
 }
