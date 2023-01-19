422f1e9afe29028ab166ecbd24d4af2e67933dd2
ailenalonso <ailu.alonso@gmail.com>
2023-01-18 23:01:35 -0300

    learning2act

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+using System;
+public class Job
+{
+    public string _company;
+    public string _jobTittle;
+    public int _startYear; 
+    public int _endYear; 
+
+    public void Display ()
+    {
+        Console.WriteLine ($"{_jobTittle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTittle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2013; 
+        job1._endYear = 2019; 
+
+        Job job2 = new Job();
+        job2._jobTittle = "Engineering Manager";
+        job2._company = "Apple";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+
+        Resume theResume = new Resume();
+        theResume._name = "Ailen Alonso";
+
+        theResume._jobs.Add(job1);
+        theResume._jobs.Add(job2);
+
+        theResume.Display();
     }
 }
